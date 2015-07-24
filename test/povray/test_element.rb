72bacc9051d9203bf800f6030ced7e073176709b
ippei94da <ippei94da@gmail.com>
2015-07-24 21:51:38 +0900

    daily change

---
+++ test/povray/test_element.rb
@@ -0,0 +1,18 @@
+#! /usr/bin/ruby
+
+require 'test/unit'
+require 'povrayutils/elementfeature.rb'
+
+class TC_Povray_Element < Test::Unit::TestCase
+  def setup
+  end
+
+  def test_radius
+    assert_equal( 0.2, CrystalCell::Povray::Element.radius('H') )
+  end
+
+  def test_color
+    assert_equal( [ 1.00, 0.80, 0.80 ], CrystalCell::Povray::Element.color('H' ) )
+  end
+end
+
