a07f893b263e1bf8ef1613a952b346827f2eacff
ippei94da <ippei94da@gmail.com>
2016-03-17 18:21:09 +0900

    daily change

---
+++ lib/crystalcell/povray/camera.rb
@@ -0,0 +1,36 @@
+#! /usr/bin/env ruby
+# coding: utf-8
+
+#
+#
+#
+class CrystalCell::Povray::Camera
+  #
+  def initialize()
+    #camera { orthographic
+    #  // orthographic       // これを入れる場合は location の前に。
+    #  location <  3.0, 3.0, 3.0 > // camera location
+    #  look_at  <  0.0, 0.0, 0.0 > // 注視点
+    #  sky      <  0.0, 0.0, 1.0 >
+    #  right    <-1.00, 0.0, 0.0 >
+    #  up       <  0.0, 1.0, 0.0 >
+    #  angle    68                 // カメラの水平方向の画角。左右合計の角度。
+    #}
+    #background {color rgb<1,1,1>}
+    #light_source{ < 4, 1, 4 > color <1,1,1> parallel point_at 0 }
+    ##default{ texture{ finish{ ambient 0.4 phong 1.0 phong_size 10 } } }
+
+    @orthographic = true
+    @location = [   3.0, 3.0, 3.0 ]
+    @look_at  = [   0.0, 0.0, 0.0 ]
+    @sky      = [   0.0, 0.0, 1.0 ]
+    @right    = [ -1.00, 0.0, 0.0 ]
+    @up       = [   0.0, 1.0, 0.0 ]
+    @angle    = 68
+    @background = [1.0, 1.0, 1.0]
+    @light_source{ < 4, 1, 4 > color <1,1,1> parallel point_at 0 }
+    #default{ texture{ finish{ ambient 0.4 phong 1.0 phong_size 10 } } }
+
+  end
+end
+
---
+++ test/povray/test_camera.rb
@@ -0,0 +1,19 @@
+#! /usr/bin/env ruby
+# coding: utf-8
+
+require "pp"
+require "helper"
+#require "test/unit"
+#require "pkg/klass.rb"
+
+class TC_Camera < Test::Unit::TestCase
+  def setup
+    @c00 = CrystalCell::Povray::Camera.new
+  end
+
+  def test_dump
+    TODO
+  end
+
+end
+
