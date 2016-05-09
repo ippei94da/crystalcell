28db6bbb40853336c89db7522d9040b2f1362833
ippei94da <ippei94da@gmail.com>
2016-05-09 15:41:50 +0900

    daily change

--- lib/crystalcell/povray/camera.rb
+++ lib/crystalcell/povray/camera.rb
@@ -25,28 +25,33 @@ class CrystalCell::Povray::Camera
 
   attr_accessor :orthographic, :location, :look_at, :sky, :right, :up, :angle
 
-  #def initialize(hash = {})
-  #  
-  #end
+  def initialize(orthographic:, location:, look_at:, sky:, right:, up:, angle:,
+                 additions: [])
+    @orthographic = orthographic
+    @location     = location    
+    @look_at      = look_at     
+    @sky          = sky         
+    @right        = right       
+    @up           = up          
+    @angle        = angle       
+    @additions    = additions       
+  end
 
   def dump(io)
-    #camera { orthographic
-    #  // orthographic       // これを入れる場合は location の前に。
-    #  location <  3.0, 3.0, 3.0 > // camera location
-    #  look_at  <  0.0, 0.0, 0.0 > // 注視点
-    #  sky      <  0.0, 0.0, 1.0 >
-    #  right    <-1.00, 0.0, 0.0 >
-    #  up       <  0.0, 1.0, 0.0 >
-    #  angle    68                 // カメラの水平方向の画角。左右合計の角度。
-    #}
+    io.puts "camera {"
+    io.puts "  orthographic" if @orthographic
+    io.printf("  location <%f, %f, %f >\n", *@location)
+    io.printf("  look_at  <%f, %f, %f >\n", *@look_at)
+    io.printf("  sky      <%f, %f, %f >\n", *@sky)
+    io.printf("  right    <%f, %f, %f >\n", *@right)
+    io.printf("  up       <%f, %f, %f >\n", *@up)
+    io.printf("  angle    %f\n"           , @angle)
+    io.puts "}"
+    @additions.each do |i|
+      io.puts i
+    end
   end
 
 end
 
-    #background {color rgb<1,1,1>}
-    #light_source{ < 4, 1, 4 > color <1,1,1> parallel point_at 0 }
-    ##default{ texture{ finish{ ambient 0.4 phong 1.0 phong_size 10 } } }
-    #@background = [1.0, 1.0, 1.0]
-    #@light_source{ < 4, 1, 4 > color <1,1,1> parallel point_at 0 }
-    ##default{ texture{ finish{ ambient 0.4 phong 1.0 phong_size 10 } } }
 
--- test/povray/test_camera.rb
+++ test/povray/test_camera.rb
@@ -8,33 +8,102 @@ require "helper"
 
 class TC_Camera < Test::Unit::TestCase
   def setup
-    @c00 = CrystalCell::Povray::Camera.new
-    @c00.orthographic = true
-    @c00.location = [   3.0, 3.0, 3.0 ]
-    @c00.look_at  = [   0.0, 0.0, 0.0 ]
-    @c00.sky      = [   0.0, 0.0, 1.0 ]
-    @c00.right    = [ -1.00, 0.0, 0.0 ]
-    @c00.up       = [   0.0, 1.0, 0.0 ]
-    @c00.angle    = 68
+    @c00 = CrystalCell::Povray::Camera.new(
+      orthographic: true,
+      location:     [   3.0, 3.0, 3.0 ],
+      look_at:      [   0.0, 0.0, 0.0 ],
+      sky:          [   0.0, 0.0, 1.0 ],
+      right:        [ -1.00, 0.0, 0.0 ],
+      up:           [   0.0, 1.0, 0.0 ],
+      angle:        68
+    )
+  end
+
+  def test_initialize
+    hash = {
+      orthographic: true,
+      location:     [   3.0, 3.0, 3.0 ],
+      look_at:      [   0.0, 0.0, 0.0 ],
+      sky:          [   0.0, 0.0, 1.0 ],
+      right:        [ -1.00, 0.0, 0.0 ],
+      up:           [   0.0, 1.0, 0.0 ],
+      angle:        68
+    }
+    @c01 = CrystalCell::Povray::Camera.new(hash)
+    assert_equal( true               , @c01.orthographic)
+    assert_equal( [   3.0, 3.0, 3.0 ], @c01.location    )
+    assert_equal( [   0.0, 0.0, 0.0 ], @c01.look_at     )
+    assert_equal( [   0.0, 0.0, 1.0 ], @c01.sky         )
+    assert_equal( [ -1.00, 0.0, 0.0 ], @c01.right       )
+    assert_equal( [   0.0, 1.0, 0.0 ], @c01.up          )
+    assert_equal( 68                 , @c01.angle       )
+  end
+
+  def test_accessor
+    assert_equal( true               , @c00.orthographic)
+    assert_equal( [   3.0, 3.0, 3.0 ], @c00.location    )
+    assert_equal( [   0.0, 0.0, 0.0 ], @c00.look_at     )
+    assert_equal( [   0.0, 0.0, 1.0 ], @c00.sky         )
+    assert_equal( [ -1.00, 0.0, 0.0 ], @c00.right       )
+    assert_equal( [   0.0, 1.0, 0.0 ], @c00.up          )
+    assert_equal( 68                 , @c00.angle       )
+
+    @c00.orthographic = false
+    assert_equal( false              , @c00.orthographic)
+  end
+
+  def test_additional
+    c10 = CrystalCell::Povray::Camera.new(
+      orthographic: true,
+      location:     [   3.0, 3.0, 3.0 ],
+      look_at:      [   0.0, 0.0, 0.0 ],
+      sky:          [   0.0, 0.0, 1.0 ],
+      right:        [ -1.00, 0.0, 0.0 ],
+      up:           [   0.0, 1.0, 0.0 ],
+      angle:        68,
+      additions:[ "background {color rgb<1,1,1>}",
+                  "light_source{ < 4, 1, 4 > color <1,1,1> parallel point_at 0 }",
+                  "default{ texture{ finish{ ambient 0.4 phong 1.0 phong_size 10 } } }",
+                ]
+    )
+    correct = <<HERE
+camera {
+  orthographic
+  location <3.000000, 3.000000, 3.000000 >
+  look_at  <0.000000, 0.000000, 0.000000 >
+  sky      <0.000000, 0.000000, 1.000000 >
+  right    <-1.000000, 0.000000, 0.000000 >
+  up       <0.000000, 1.000000, 0.000000 >
+  angle    68.000000
+}
+background {color rgb<1,1,1>}
+light_source{ < 4, 1, 4 > color <1,1,1> parallel point_at 0 }
+default{ texture{ finish{ ambient 0.4 phong 1.0 phong_size 10 } } }
+HERE
+    io = StringIO.new
+    c10.dump(io)
+    io.rewind
+    result = io.read
+    assert_equal(correct, result)
   end
 
   def test_dump
     correct = <<HERE
-camera { orthographic
-  // orthographic
-  location <  3.0, 3.0, 3.0 >
-  look_at  <  0.0, 0.0, 0.0 >
-  sky      <  0.0, 0.0, 1.0 >
-  right    <-1.00, 0.0, 0.0 >
-  up       <  0.0, 1.0, 0.0 >
-  angle    68
+camera {
+  orthographic
+  location <3.000000, 3.000000, 3.000000 >
+  look_at  <0.000000, 0.000000, 0.000000 >
+  sky      <0.000000, 0.000000, 1.000000 >
+  right    <-1.000000, 0.000000, 0.000000 >
+  up       <0.000000, 1.000000, 0.000000 >
+  angle    68.000000
 }
 HERE
-    assert_equal(correct, @c00.dump)
+    io = StringIO.new
+    @c00.dump(io)
+    io.rewind
+    result = io.read
+    assert_equal(correct, result)
   end
 
 end
-
-    #background {color rgb<1,1,1>}
-    #light_source{ < 4, 1, 4 > color <1,1,1> parallel point_at 0 }
-    ##default{ texture{ finish{ ambient 0.4 phong 1.0 phong_size 10 } } }
