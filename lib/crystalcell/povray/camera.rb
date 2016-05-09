0e029c8d0f870ae046a199f78b6d47fc445ad16f
ippei94da <ippei94da@gmail.com>
2016-05-09 14:49:05 +0900

    daily change

--- lib/crystalcell/povray/camera.rb
+++ lib/crystalcell/povray/camera.rb
@@ -6,7 +6,6 @@
 #
 class CrystalCell::Povray::Camera
   #
-  def initialize()
     #camera { orthographic
     #  // orthographic       // これを入れる場合は location の前に。
     #  location <  3.0, 3.0, 3.0 > // camera location
@@ -16,10 +15,6 @@ class CrystalCell::Povray::Camera
     #  up       <  0.0, 1.0, 0.0 >
     #  angle    68                 // カメラの水平方向の画角。左右合計の角度。
     #}
-    #background {color rgb<1,1,1>}
-    #light_source{ < 4, 1, 4 > color <1,1,1> parallel point_at 0 }
-    ##default{ texture{ finish{ ambient 0.4 phong 1.0 phong_size 10 } } }
-
     #@orthographic = true
     #@location = [   3.0, 3.0, 3.0 ]
     #@look_at  = [   0.0, 0.0, 0.0 ]
@@ -27,10 +22,31 @@ class CrystalCell::Povray::Camera
     #@right    = [ -1.00, 0.0, 0.0 ]
     #@up       = [   0.0, 1.0, 0.0 ]
     #@angle    = 68
+
+  attr_accessor :orthographic, :location, :look_at, :sky, :right, :up, :angle
+
+  #def initialize(hash = {})
+  #  
+  #end
+
+  def dump(io)
+    #camera { orthographic
+    #  // orthographic       // これを入れる場合は location の前に。
+    #  location <  3.0, 3.0, 3.0 > // camera location
+    #  look_at  <  0.0, 0.0, 0.0 > // 注視点
+    #  sky      <  0.0, 0.0, 1.0 >
+    #  right    <-1.00, 0.0, 0.0 >
+    #  up       <  0.0, 1.0, 0.0 >
+    #  angle    68                 // カメラの水平方向の画角。左右合計の角度。
+    #}
+  end
+
+end
+
+    #background {color rgb<1,1,1>}
+    #light_source{ < 4, 1, 4 > color <1,1,1> parallel point_at 0 }
+    ##default{ texture{ finish{ ambient 0.4 phong 1.0 phong_size 10 } } }
     #@background = [1.0, 1.0, 1.0]
     #@light_source{ < 4, 1, 4 > color <1,1,1> parallel point_at 0 }
     ##default{ texture{ finish{ ambient 0.4 phong 1.0 phong_size 10 } } }
-
-  end
-end
 
--- test/povray/test_camera.rb
+++ test/povray/test_camera.rb
@@ -9,11 +9,32 @@ require "helper"
 class TC_Camera < Test::Unit::TestCase
   def setup
     @c00 = CrystalCell::Povray::Camera.new
+    @c00.orthographic = true
+    @c00.location = [   3.0, 3.0, 3.0 ]
+    @c00.look_at  = [   0.0, 0.0, 0.0 ]
+    @c00.sky      = [   0.0, 0.0, 1.0 ]
+    @c00.right    = [ -1.00, 0.0, 0.0 ]
+    @c00.up       = [   0.0, 1.0, 0.0 ]
+    @c00.angle    = 68
   end
 
   def test_dump
-    #TODO
+    correct = <<HERE
+camera { orthographic
+  // orthographic
+  location <  3.0, 3.0, 3.0 >
+  look_at  <  0.0, 0.0, 0.0 >
+  sky      <  0.0, 0.0, 1.0 >
+  right    <-1.00, 0.0, 0.0 >
+  up       <  0.0, 1.0, 0.0 >
+  angle    68
+}
+HERE
+    assert_equal(correct, @c00.dump)
   end
 
 end
 
+    #background {color rgb<1,1,1>}
+    #light_source{ < 4, 1, 4 > color <1,1,1> parallel point_at 0 }
+    ##default{ texture{ finish{ ambient 0.4 phong 1.0 phong_size 10 } } }
