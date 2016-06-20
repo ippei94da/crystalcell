fc02f696ab72a9ddba65e3d53173ff7928063e34
ippei94da <ippei94da@gmail.com>
2016-06-21 03:52:15 +0900

    daily change

--- lib/crystalcell/povray/color.rb
+++ lib/crystalcell/povray/color.rb
@@ -11,33 +11,30 @@ class CrystalCell::Povray::Color
   def initialize()
   end
 
-  # R,G,B のいずれかの色の theta における強度を返す。
-  def self.round_color(theta)
+  # 色相環(color circle) 上での色を、与えられた角度に対して返す。
+  # 強度は [r, g, b] の配列で、0.0<=x<=1.0 の実数。
+  def self.circle_color(theta)
+    theta = theta % (2*PI); #-2*pi 〜 +2*pi の範囲に入る
 
-    #theta = theta % (2*PI); #-2*pi 〜 +2*pi の範囲に入る
-
-    #if (0.0/6.0)*(2.0*PI) <= theta && theta < (1.0/6.0)*(2.0*PI))
-    #  r = 0.0
-    #  g = theta/((1/6)*(2*PI))
-    #elsif (0.0/6.0)*(2.0*PI) <= theta && theta < (1.0/6.0)*(2.0*PI))
-    #    intensity = 1;
-
-    #  ((3/6)*(2*PI) , (4/6)*(2*PI))
-    #    intensity = 4-theta/((1/6)*(2*PI)) ;
-
-    #  ((4/6)*(2*PI) , (6/6)*(2*PI))
-    #    intensity = 0;
-    #intensity
+    r = self.trapezoidal_wave(theta, (4.0/6.0) * 2.0 * PI)
+    g = self.trapezoidal_wave(theta, (0.0/6.0) * 2.0 * PI)
+    b = self.trapezoidal_wave(theta, (2.0/6.0) * 2.0 * PI)
+    [r, g, b]
   end
 
-  def self.intensity(theta, shift)
-    theta /= (2*PI)
-    theta -= theta.to_i
-    if (0.0/6.0)*(2.0*PI) <= theta && theta < (1.0/6.0)*(2.0*PI))
+  # 台形波
+  def self.trapezoidal_wave(theta, phase)
+    x = (theta - phase)/ (2.0*PI)
+    x = x -  x.floor
+    if (0.0/6.0) <= x && x < (1.0/6.0)
+      return x * 6.0
+    elsif (1.0/6.0) <= x && x < (3.0/6.0)
       return 1.0
-    elsif (1.0/6.0)*(2.0*PI) <= theta && theta < (2.0/6.0)*(2.0*PI))
-      return 1.0 -theta
-    HERE
+    elsif (3.0/6.0) <= x && x < (4.0/6.0)
+      return 4.0 - x * 6.0
+    elsif (4.0/6.0) <= x && x < (6.0/6.0)
+      return 0.0
+    end
   end
 
 end
